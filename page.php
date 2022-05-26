8aa9ff40bb4e8e27f0de1d7f2bb6dbe9a542ae38
Francois <contact.fpweb@gmail.com>
2022-05-26 16:54:08 +0200

    added sub page with breadcrumb

--- page.php
+++ page.php
@@ -3,8 +3,21 @@ get_header();
 while (have_posts()) {
     the_post(); ?>
     <main class="container">
-        <h2><?php the_title() ?></h2>
-        <?php the_content() ?>
+        <?php
+        $parentId = wp_get_post_parent_id(get_the_ID());
+        if ($parentId) {
+        ?>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo get_permalink($parentId) ?>"><small><?php echo get_the_title($parentId); ?></small></a></li>
+                    <li>|</li>
+                    <li><small><?php the_title(); ?></small></li>
+                </ul>
+            </nav>
+        <?php }
+        ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
     </main>
 <?php }
 get_footer();
